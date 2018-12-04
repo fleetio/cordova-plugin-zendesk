4ad6b66e2682410ac71357d7c35f811a41a37179
Cameron Stanley <cameronstanley23@gmail.com>
2018-12-04 15:50:36 -0600

    More fixes for JS wrapper

--- www/Zendesk.js
+++ www/Zendesk.js
@@ -3,16 +3,15 @@ var exec = require("cordova/exec");
 function Zendesk() {}
 
 Zendesk.prototype.initialize = function(appId, clientId, zendeskUrl, successCallback, errorCallback) {
-  exec(successCallback, errorCallback, appId, clientId, zendeskUrl);
+  exec(successCallback, errorCallback, 'Zendesk', 'initialize', [appId, clientId, zendeskUrl]);
 }
 
 Zendesk.prototype.setAnonymousIdentity = function(name, email, successCallback, errorCallback) {
-  exec(successCallback, errorCallback, name, email);
+  exec(successCallback, errorCallback, 'Zendesk', 'setAnonymousIdentity', [name, email]);
 }
 
 Zendesk.prototype.showHelpCenter = function(successCallback, errorCallback) {
-  exec(successCallback, errorCallback);
+  exec(successCallback, errorCallback, 'Zendesk', 'showHelpCenter', []);
 }
-
 var Zendesk = new Zendesk();
 module.exports = Zendesk;
