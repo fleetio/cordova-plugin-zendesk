e39f592db9475a8a9af3a6e7a0741e19b0301d38
Cameron Stanley <cameronstanley23@gmail.com>
2018-12-04 14:56:19 -0600

    Small tweak to JS wrapper

--- www/Zendesk.js
+++ www/Zendesk.js
@@ -1,15 +1,17 @@
+var exec = require("cordova/exec");
+
 function Zendesk() {}
 
 Zendesk.prototype.initialize = function(appId, clientId, zendeskUrl, successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, appId, clientId, zendeskUrl);
+  exec(successCallback, errorCallback, appId, clientId, zendeskUrl);
 }
 
 Zendesk.prototype.setAnonymousIdentity = function(name, email, successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback, name, email);
+  exec(successCallback, errorCallback, name, email);
 }
 
 Zendesk.prototype.showHelpCenter = function(successCallback, errorCallback) {
-  cordova.exec(successCallback, errorCallback);
+  exec(successCallback, errorCallback);
 }
 
 var Zendesk = new Zendesk();
