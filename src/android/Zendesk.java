ef10a8faa5167e4122cf19e22fac3f612752df7f
Cameron Stanley <cameronstanley23@gmail.com>
2018-12-07 07:33:37 -0600

    Implement Android methods for initialization and setting identity

--- src/android/Zendesk.java
+++ src/android/Zendesk.java
@@ -1,8 +1,13 @@
 package com.rarestep.zendesk;
 
+import android.content.Context;
+
 import org.apache.cordova.*;
 
 import org.json.JSONException;
+
+import zendesk.core.AnonymousIdentity;
+import zendesk.core.Identity;
 
 public class Zendesk extends CordovaPlugin {
   private static final String ACTION_INITIALIZE = "initialize";
@@ -15,9 +20,21 @@ public class Zendesk extends CordovaPlugin {
   public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext)
     throws JSONException {
     if (ACTION_INITIALIZE.equals(action)) {
-        
-    } else if (ACTION_SET_ANONYMOUS_IDENTITY.equals(action)) {
+      String appId = args.getString(0);
+      String clientId = args.getString(1);
+      String zendeskUrl = args.getString(2);
 
+      zendesk.core.Zendesk.INSTANCE.init(this.getContext(), zendeskUrl, appId, clientId);
+    } else if (ACTION_SET_ANONYMOUS_IDENTITY.equals(action)) {
+      String name = args.getString(0);
+      String email = args.getString(1);
+
+      Identity identity = new AnonymousIdentity.Builder()
+        .withNameIdentifier(name)
+        .withEmailIdentifier(email)
+        .build();
+
+      zendesk.core.Zendesk.INSTANCE.setIdentity(identity);
     } else if (ACTION_SHOW_HELP_CENTER.equals(action)) {
 
     } else if (ACTION_SHOW_TICKET_REQUEST.equals(action)) {
@@ -25,9 +42,15 @@ public class Zendesk extends CordovaPlugin {
     } else if (ACTION_SHOW_USER_TICKETS.equals(action)) {
 
     } else {
+      callbackContext.error("Invalid action: " + action);
       return false;
     }
 
+    callbackContext.success();
     return true;
+  }
+
+  private Context getContext() {
+    return this.cordova.getActivity().getApplicationContext();
   }
 }
