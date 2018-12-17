fa88ba4fc2f4f6f73379d74687c9cc26ff346d18
Cameron Stanley <cameronstanley23@gmail.com>
2018-12-17 16:13:41 -0600

    Android fixes

--- src/android/Zendesk.java
+++ src/android/Zendesk.java
@@ -3,12 +3,19 @@ package com.rarestep.zendesk;
 import android.content.Context;
 import android.content.Intent;
 
+import com.fleetio.go_app.MainActivity;
+import com.zendesk.logger.Logger;
+
+import junit.framework.Test;
+
 import org.apache.cordova.*;
 
 import org.json.JSONException;
 
+import zendesk.commonui.UiConfig;
 import zendesk.core.AnonymousIdentity;
 import zendesk.core.Identity;
+import zendesk.support.Support;
 import zendesk.support.guide.HelpCenterActivity;
 import zendesk.support.request.RequestActivity;
 import zendesk.support.requestlist.RequestListActivity;
@@ -29,6 +36,7 @@ public class Zendesk extends CordovaPlugin {
       String zendeskUrl = args.getString(2);
 
       zendesk.core.Zendesk.INSTANCE.init(this.getContext(), zendeskUrl, appId, clientId);
+      Support.INSTANCE.init(zendesk.core.Zendesk.INSTANCE);
     } else if (ACTION_SET_ANONYMOUS_IDENTITY.equals(action)) {
       String name = args.getString(0);
       String email = args.getString(1);
@@ -40,14 +48,11 @@ public class Zendesk extends CordovaPlugin {
 
       zendesk.core.Zendesk.INSTANCE.setIdentity(identity);
     } else if (ACTION_SHOW_HELP_CENTER.equals(action)) {
-      Intent intent = HelpCenterActivity.builder().intent(this.getContext());
-      this.cordova.getActivity().startActivity(intent);
+      HelpCenterActivity.builder().show(this.getContext());
     } else if (ACTION_SHOW_TICKET_REQUEST.equals(action)) {
-      Intent intent = RequestActivity.builder().intent(this.getContext());
-      this.cordova.getActivity().startActivity(intent);
+      RequestActivity.builder().show(this.getContext());
     } else if (ACTION_SHOW_USER_TICKETS.equals(action)) {
-      Intent intent = RequestActivity.builder().intent(this.getContext());
-      RequestListActivity.builder().intent(this.getContext());
+      RequestListActivity.builder().show(this.getContext());
     } else {
       callbackContext.error("Invalid action: " + action);
       return false;
